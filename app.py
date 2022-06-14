83cc316ebe79f205163c9ccaef686238e393b5b0
137WYC042 <106513942+137WYC042@users.noreply.github.com>
2022-06-14 15:25:48 +0800

    update geocoding

--- app.py
+++ app.py
@@ -58,3 +58,26 @@ def get_data():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
+#verify company address
+apikey = os.environ.get()
+import nlp
+import pandas as pd
+import googlemaps
+gmaps = googlemaps.Client(key=apikey)
+
+#input an address
+address= input("Enter address: ")
+
+# Geocoding an address
+geocode_result = gmaps.geocode(address)
+
+if geocode_result == []:
+    print ("This address is invalid")
+else:
+    geocode_result= geocode_result[0]
+    if 'plus_code' in geocode_result:
+        print("The Company address is valid")
+    else:
+        print("This address is vague, This job invite is likely a scam")
